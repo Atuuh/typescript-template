c7b5fd20006a18117a39bf892af5988be7d83752
Atuuh <not-another-one@hotmail.com>
2020-10-03 15:54:39 +0100

    Initial Commit

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import { apple } from '.'
+
+describe('Sample test', () => {
+    test('apple', () => {
+        expect(apple.type).toBe('fruit')
+    })
+})
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export const apple = { type: 'fruit' }
+
+console.log('apple', apple)
+
+const x = 'test'
+
+console.log('x', x)
